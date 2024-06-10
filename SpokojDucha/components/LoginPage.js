3bb82bd77415a1018e5cb110dbd2cbbf8548fcab
JuliaTrzebuchowska <jtrzebuchowska1@gamil.com>
2024-06-10 21:48:36 +0200

    Login Page css

--- SpokojDucha/components/LoginPage.js
+++ SpokojDucha/components/LoginPage.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, View, TextInput, Button, Alert } from "react-native";
+import { StyleSheet, View, TextInput, Button, Alert, Text, TouchableOpacity } from "react-native";
 import { signIn } from "../microservices/auth/Auth";
 
 const LoginPage = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
   const handleLogin = () => {
     if (!email.trim() || !password.trim()) {
       Alert.alert("Login Error", "Email and password are required.");
@@ -30,6 +30,8 @@ const LoginPage = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.formContainer}>
+      <Text style={styles.title}>Logowanie</Text>
       <View style={styles.loginTextSection}>
         <TextInput
           placeholder="Email"
@@ -37,19 +39,25 @@ const LoginPage = ({ navigation }) => {
           value={email}
           onChangeText={setEmail}
         />
-        <TextInput
-          placeholder="Password"
-          style={styles.inputText}
-          secureTextEntry={true}
-          value={password}
-          onChangeText={setPassword}
-        />
+        <View style={styles.passwordContainer}>
+          <TextInput
+            placeholder="Hasło"
+            style={styles.inputText}
+            secureTextEntry={secureTextEntry}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity onPress={() => setSecureTextEntry(!secureTextEntry)} style={styles.showPasswordContainer}>
+            <Text style={styles.showPassword}>{secureTextEntry ? "Pokaż" : "Ukryj"}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
-      <View style={styles.loginButtonSection}>
-        <Button onPress={handleLogin} title="Login" />
       </View>
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Logowanie</Text>
+      </TouchableOpacity>
     </View>
+
   );
 };
 
@@ -57,28 +65,64 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    
+    
+    justifyContent: 'space-between',
+    padding: 20,
   },
-  loginTextSection: {
+  formContainer: {
+    paddingTop:25,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
     marginBottom: 20,
   },
+
   inputText: {
     borderWidth: 1,
-    borderColor: "gray",
+    borderColor: "#ccc",
+    backgroundColor: "#f5f5f5",
     padding: 10,
     marginBottom: 10,
-    width: 200,
-  },
-  loginButtonSection: {
+    borderRadius: 5,
     width: "100%",
-    height: "30%",
-    justifyContent: "center",
+  },
+  loginTextSection: {
+    width: "100%",
+    marginBottom: 20,
+  },
+  passwordContainer: {
+
+    flexDirection: "row",
     alignItems: "center",
+    width: "100%",
+
+  },
+  showPasswordContainer: {
+    position: "absolute",
+    right: 10,
+    paddingRight: 10,
+
+  },
+  showPassword: {
+    color: "#5DB075",
+
   },
   loginButton: {
-    backgroundColor: "blue",
-    color: "white",
+    backgroundColor: "#5DB075",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+    alignItems: "center",
+    width: "100%",
+    marginBottom:20,
+  },
+  loginButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
