1c8e683cf1fd7ae428f43599b19b9382963be67b
JuliaTrzebuchowska <jtrzebuchowska1@gamil.com>
2024-06-10 23:07:58 +0200

    WelcomePage Css

--- SpokojDucha/components/WelcomePage.js
+++ SpokojDucha/components/WelcomePage.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
 
 const WelcomePage = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the App!</Text>
-      <Button title="Add Cementery" onPress={() => navigation.navigate('Add cementery')} />
-      <Button title="Add Decedent" onPress={() => navigation.navigate('Add decedent')} />
-      <Button title="Log Out" onPress={() => navigation.navigate('Home')} />
-    </View>
+   
+    <Text style={styles.welcomeText}>Witaj w aplikacji!</Text>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Add cementery')}>
+      <Text style={styles.buttonText}>Dodaj cmentarz</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Add decedent')}>
+      <Text style={styles.buttonText}>Dodaj nagrobek</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Search')}>
+      <Text style={styles.buttonText}>Szukaj</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={[styles.button, {marginTop: 100}]} onPress={() => navigation.navigate('Home')}>
+      <Text style={styles.buttonText}>Wyloguj</Text>
+    </TouchableOpacity>
+  </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
   },
-  text: {
+ 
+  backText: {
+    color: '#5DB075',
+    fontSize: 16,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  welcomeText: {
     fontSize: 24,
     marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#5DB075',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+    alignItems: 'center',
+    width: '80%',
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
